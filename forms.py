8df7b9f4c8931b9d9eb842e209fc7e71ffa3557c
Filipster <smart.filip48@gmail.com>
2019-05-03 21:47:21 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,25 +3,33 @@ import pygame
 import constants as cst
 
 class Form:
-    def __init__(self,center,color,surface):
+    def __init__(self,center,color,surface,board):
         self.center=center
         self.surface=surface
         self.color=color
+        self.board = board
         self.blocks=[]     
     def rotate(self):
         return
     def down(self,nr=1):
         #print(len(self.blocks))
-        #for b in self.blocks:
+        ok = False
+        for b in self.blocks:
+            if not b.can_down():
+                return
             
         for b in self.blocks:
             pygame.draw.rect(b.surface, cst.BLACK, b.rect)
         for b in self.blocks:
-            
             b.down(nr,False)
 
     def left(self,nr=1):
         #print(len(self.blocks))
+        ok = False
+        for b in self.blocks:
+            if(!b.can_left()):
+                return
+        
         for b in self.blocks:
             pygame.draw.rect(b.surface, cst.BLACK, b.rect)
         for b in self.blocks:
@@ -29,83 +37,93 @@ class Form:
 
     def right(self,nr=1):
         #print(len(self.blocks))
+        ok = False
+        for b in self.blocks:
+            if(!b.can_right()):
+                return
+        
         for b in self.blocks:
             pygame.draw.rect(b.surface, cst.BLACK, b.rect)
         for b in self.blocks:
             b.right(nr,False)
+    
+    def rot_ck: #clock-wise rotation
+        for b in self.blocks:
+            x = b.coords[0]
+            y = b.coords[1]
 
 class Square(Form):
-    def __init__(self,center,surface):
-        Form.__init__(self,center,cst.YELLOW,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):
+        Form.__init__(self,center,cst.YELLOW,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
     # Rotate sta
 
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surfac, boarde))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[1]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
     # Rotate sta
     
 class L1(Form):
-    def __init__(self,center,surface):
-        Form.__init__(self,center,cst.BLUE,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):
+        Form.__init__(self,center,cst.BLUE,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=2
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[1]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
 
 class L2(Form):
-    def __init__(self,center,surface):
-        Form.__init__(self,center,cst.ORANGE,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):
+        Form.__init__(self,center,cst.ORANGE,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=2
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[1]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))  
+        self.blocks.append(block.Block(self.color,center,False,surface, board))  
         
 class Fulger1(Form):
-    def __init__(self,center,surface):
-        Form.__init__(self,center,cst.RED,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):
+        Form.__init__(self,center,cst.RED,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=1
         center[1]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
 
 class Fulger2(Form):
-    def __init__(self,center,surface):
-        Form.__init__(self,center,cst.GREEN,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):
+        Form.__init__(self,center,cst.GREEN,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
         center[1]-=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
   
 
 class Line(Form):
-    def __init__(self,center,surface):  
-        Form.__init__(self,center,cst.CYAN,surface)
-        self.blocks.append(block.Block(self.color,center,False,surface))
+    def __init__(self,center,surface, board):  
+        Form.__init__(self,center,cst.CYAN,surface, board)
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
         center[0]+=1
-        self.blocks.append(block.Block(self.color,center,False,surface))
+        self.blocks.append(block.Block(self.color,center,False,surface, board))
 
 
 if("__main__"==__name__):## Test ONLY
