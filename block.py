9d02d370a8d13281673d92443e0545b4b5fc0068
Filipster <smart.filip48@gmail.com>
2019-05-03 20:47:15 +0300

    Fixed it all

--- block.py
+++ block.py
@@ -3,18 +3,8 @@ import constants as cst
 
 
 def get_position(x, y):
-<<<<<<< HEAD
-<<<<<<< HEAD
-	pos_x = cst.BLOCK_SIZE*x + cst.MARGIN
-	pos_y = cst.BLOCK_SIZE*y + cst.MARGIN
-=======
-	x = cst.BLOCK_SIZE*x + cst.MARGIN
-	y = cst.BLOCK_SIZE*y + cst.MARGIN
->>>>>>> parent of 3736848... Moves
-=======
-	x = cst.BLOCK_SIZE*x + cst.MARGIN
-	y = cst.BLOCK_SIZE*y + cst.MARGIN
->>>>>>> parent of 3736848... Moves
+	x = cst.BLOCK_SIZE*x
+	y = cst.BLOCK_SIZE*y
 	return x,y
 
 class Block:
@@ -22,16 +12,31 @@ class Block:
         self.color = color
         self.coords = coords
         self.fixed = fixed
+        self.surface = surface
         x,y = get_position(coords[0],coords[1])
-<<<<<<< HEAD
-<<<<<<< HEAD
-        self.rect = pygame.Rect(x,y,cst.BLOCK_SIZE,cst.BLOCK_SIZE)
-=======
-        self.rect = pygame.Rect(x,y,cst.BLOCK_SIZE-1,cst.BLOCK_SIZE-1)
->>>>>>> parent of 3736848... Moves
-=======
-        self.rect = pygame.Rect(x,y,cst.BLOCK_SIZE-1,cst.BLOCK_SIZE-1)
->>>>>>> parent of 3736848... Moves
+        self.rect = pygame.Rect(x+1,y+1,cst.BLOCK_SIZE-1,cst.BLOCK_SIZE-1)
         pygame.draw.rect(surface, color, self.rect)
 
-    
+    def down(self,nr=1,f=True):
+        if(f):
+            pygame.draw.rect(self.surface, cst.BLACK, self.rect)
+        self.coords[1]+=1
+        x,y = get_position(0,1)
+        self.rect.move_ip(x,y)
+        pygame.draw.rect(self.surface, self.color, self.rect)
+
+    def left(self,nr=1,f=True):
+        if(f):
+            pygame.draw.rect(self.surface, cst.BLACK, self.rect)
+        self.coords[0]-=1
+        x,y = get_position(-1,0)
+        self.rect.move_ip(x,y)
+        pygame.draw.rect(self.surface, self.color, self.rect)
+
+    def right(self,nr=1,f=True):
+        if(f):
+            pygame.draw.rect(self.surface, cst.BLACK, self.rect)
+        self.coords[0]+=1
+        x,y = get_position(1,0)
+        self.rect.move_ip(x,y)
+        pygame.draw.rect(self.surface, self.color, self.rect)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import constants as cst
 import block 
+import forms
 
 def exit():
     print("BYE") #debug
@@ -15,46 +16,44 @@ class Tetris:
         self.background = pygame.Surface(self.screen.get_size())
         self.background.fill(cst.GRAY)  
         pygame.display.set_caption("Tetrix")
+        self.foreground = pygame.Surface(cst.BOARD_SIZE)
+        self.foreground.fill(cst.BLACK)
+        self.screen.blit(self.background,(0,0))
+        self.screen.blit(self.foreground,(cst.MARGIN,cst.MARGIN))
 
     def draw(self):
-        foreground = pygame.Surface(cst.BOARD_SIZE)
-        foreground.fill(cst.BLACK)
-        self.screen.blit(self.background,(0,0))
-        self.screen.blit(foreground,(cst.MARGIN,cst.MARGIN))
         pygame.display.flip()
         
     def run(self):
-        self.draw()
-<<<<<<< HEAD
-<<<<<<< HEAD
-        while True:
-=======
-=======
->>>>>>> parent of 3736848... Moves
-        s=forms.Line([3,3],cst.BLUE,self.foreground)
+        #s=forms.Line([3,3],cst.BLUE,self.foreground)
         #b=block.Block(cst.BLUE,[3,3],False,self.foreground)
+        self.ON = True
+        self.currentForm = forms.Square([0, 0], self.foreground)
+        self.draw()
         while self.ON:
->>>>>>> parent of 3736848... Moves
+
+            
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
                     exit()
                 #ESC = exit()
                 if(event.type==pygame.KEYDOWN):
                     if(event.key==27):
-<<<<<<< HEAD
-                        exit()
-=======
                         self.ON=False
                         break;
-<<<<<<< HEAD
->>>>>>> parent of 3736848... Moves
-=======
->>>>>>> parent of 3736848... Moves
-        
-
-
-
+                    if(event.key==274):
+                        #b.down()
+                        self.currentForm.down()
+                    if(event.key==275):
+                        #b.right()
+                        self.currentForm.right()
+                    if(event.key==276):
+                        #b.left()
+                        self.currentForm.left()
+                    #print (event.key)
             self.draw()
+        exit()
+
 
 def main():
     game = Tetris()
