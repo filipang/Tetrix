73748045aa49237c4decf3479d7de6a8e44e5662
Cristi_Sima <cristiansimache@gmail.com>
2019-05-05 18:13:45 +0300

    Temp Fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def exit():
     pygame.quit()
     sys.exit()
 
-class Main:
+class Main_Thread:
     def __init__(self):
         pygame.init()
         self.Game=Game.Tetris()
@@ -27,9 +27,28 @@ class Main:
         self.Game.ON=False
         print("Score:",self.Game.Levels)
         time.sleep(0.000000001)
+
+class Main_NoThread:
+    def __init__(self):
+        pygame.init()
+        self.Game=Game.Tetris()
+    def Start(self):
+        self.Game.run()
+    def Wait(self):
+        try:
+            #print("IN")
+            while self.Game.ON:
+                self.Game.run()
+            #print("EXIT")
+        except KeyboardInterrupt:
+            None
+            
+        self.Game.ON=False
+        print("Score:",self.Game.Levels)
+        time.sleep(0.000000001)
     
 if(__name__=="__main__"):        
-    M=Main()
+    M=Main_NoThread()
     M.Start()
     M.Wait()
     exit()
